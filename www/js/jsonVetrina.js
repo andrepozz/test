e64f261eb3a7e3ae802c74c3870dcb06be5edca6
andrepozz <andrepozz@inwind.it>
2014-03-13 10:21:18 +0100

    jquery + mobile + barcode

---
+++ www/js/jsonVetrina.js
@@ -0,0 +1,36 @@
+[
+    {
+    "idEsercizio": "16",
+    "NomeEsercizio": "Mitchumm",
+    "Descrizione": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Indirizzo": "via Milano 11",
+    "Citta": "Como",
+    "Cap": "22100",
+    "Latitudine": "45.805650500000",
+    "Longitudine": "9.085535300000",
+    "Orario": {
+        "lunedi": "10.00-17.00",
+        "giovedi": "11.00-16.00",
+        "domenica": "chiuso",
+        "martedi": "chiuso",
+        "mercoledi": "chiuso",
+        "venerdi": "chiuso",
+        "sabato": "chiuso"
+    },
+    "Website": "www.negozio1.it",
+    "Telefono": "3334554456",
+    "Email": "info@negozio.it",
+    "Facebook": "https://www.facebook.com/MitchummIndustries",
+    "Twitter": "https://twitter.com/MITCHUMM",
+    "Foursquare": "https://it.foursquare.com/v/mitchumm-store/4fbbba82e4b0182c68e66fe1",
+    "Logo": "09.jpg",
+    "Parcheggio": {
+        "idParcheggio": "8",
+        "nomeParcheggio": "Autosilo 1",
+        "indirizzoParcheggio": "viale Lecco 9",
+        "latitudineParcheggio": "45.810391500000",
+        "longitudineParcheggio": "9.086423300000"
+    },
+    "Statistiche": "si"
+	}
+	]
