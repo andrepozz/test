39b034a02a4c67f40b12908b554b2cba7a7bad8b
andrepozz <andrepozz@inwind.it>
2014-03-19 12:01:41 +0100

    test

--- www/js/16.js
+++ www/js/16.js
@@ -1,50 +1,51 @@
 [
-{
-    "idEsercizio": "16",
-    "NomeEsercizio": "Mitchumm",
-    "Descrizione": "Questa è la descrizione del negozio in ITALIANO",
-    "Indirizzo": "via Milano 11",
-    "Citta": "Como",
-    "Cap": "22100",
-    "Latitudine": "45.805650500000",
-    "Longitudine": "9.085535300000",
-    "Orario": {
-        "lunedi": "10.00-17.00",
-        "giovedi": "11.00-16.00",
-        "domenica": "chiuso",
-        "martedi": "chiuso",
-        "mercoledi": "chiuso",
-        "venerdi": "chiuso",
-        "sabato": "chiuso"
-    },
-    "Website": "www.negozio1.it",
-    "Telefono": "3334554456",
-    "Email": "info@negozio.it",
-    "Facebook": "https://www.facebook.com/MitchummIndustries",
-    "Twitter": "https://twitter.com/MITCHUMM",
-    "Foursquare": "https://it.foursquare.com/v/mitchumm-store/4fbbba82e4b0182c68e66fe1",
-    "Logo": "09.jpg",
-    "Parcheggio": {
-        "idParcheggio": "8",
-        "nomeParcheggio": "Autosilo 1",
-        "indirizzoParcheggio": "viale Lecco 9",
-        "latitudineParcheggio": "45.810391500000",
-        "longitudineParcheggio": "9.086423300000"
-    },
-    "Statistiche": "si",
-    "Distanze": {
-        "18": {
-            "idEsercizio": "18",
-            "nomeEsercizio": "Tessabit",
-            "distance": "186",
-            "time": "134"
+    {
+        "idEsercizio": "16",
+        "NomeEsercizio": "Mitchumm",
+        "Descrizione": "This is the shop description in ENGLISH",
+        "LinguaDescrizione": "en",
+        "Indirizzo": "via Milano 11",
+        "Citta": "Como",
+        "Cap": "22100",
+        "Latitudine": "45.805650500000",
+        "Longitudine": "9.085535300000",
+        "Orario": {
+            "lunedi": "10.00-17.00",
+            "giovedi": "11.00-16.00",
+            "domenica": "chiuso",
+            "martedi": "chiuso",
+            "mercoledi": "chiuso",
+            "venerdi": "chiuso",
+            "sabato": "chiuso"
         },
-        "154": {
-            "idEsercizio": "154",
-            "nomeEsercizio": "Bar Gelateria Tentazioni",
-            "distance": "161",
-            "time": "116"
+        "Website": "www.negozio1.it",
+        "Telefono": "3334554456",
+        "Email": "info@negozio.it",
+        "Facebook": "https://www.facebook.com/MitchummIndustries",
+        "Twitter": "https://twitter.com/MITCHUMM",
+        "Foursquare": "https://it.foursquare.com/v/mitchumm-store/4fbbba82e4b0182c68e66fe1",
+        "Logo": "09.jpg",
+        "Parcheggio": {
+            "idParcheggio": "8",
+            "nomeParcheggio": "Autosilo 1",
+            "indirizzoParcheggio": "viale Lecco 9",
+            "latitudineParcheggio": "45.810391500000",
+            "longitudineParcheggio": "9.086423300000"
+        },
+        "Statistiche": "si",
+        "Distanze": {
+            "18": {
+                "idEsercizio": "18",
+                "nomeEsercizio": "Tessabit",
+                "distance": "186",
+                "time": "134"
+            },
+            "154": {
+                "idEsercizio": "154",
+                "nomeEsercizio": "Bar Gelateria Tentazioni",
+                "distance": "161",
+                "time": "116"
+            }
         }
     }
-}
 ]
